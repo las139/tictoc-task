deee46c182cfa58bb57dbf98162cf07570fedb14
las139 <qqwer153@gmail.com>
2024-02-10 13:41:21 +0900

    refactor: 지오코딩 api 호출 결과 not found 예외처리 추가

--- src/main/java/com/lsm/task/service/GeoCodingService.java
+++ src/main/java/com/lsm/task/service/GeoCodingService.java
@@ -1,5 +1,7 @@
 package com.lsm.task.service;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import com.lsm.task.dto.GetCoordinatesResponse;
 public class GeoCodingService {
     private static final String VERSION = "2.0";
     private static final String EPSG = "epsg:4326";
+    private static final String STATUS_NOT_FOUND = "NOT_FOUND";
 
     private final RestTemplate restTemplate;
 
@@ -33,7 +36,11 @@ public class GeoCodingService {
             "&refine=true&simple=false&format=json&type=road&key=" + apiKey;
 
         try {
-            return restTemplate.getForObject(requestUrl, GetCoordinatesResponse.class);
+            GetCoordinatesResponse response = restTemplate.getForObject(requestUrl, GetCoordinatesResponse.class);
+            if (Objects.requireNonNull(response).getResponse().getStatus().equals(STATUS_NOT_FOUND)) {
+                throw new IllegalArgumentException("위,경도에 해당하는 주소를 찾지 못했습니다.");
+            }
+            return response;
         } catch (RestClientException e) {
             throw new RestClientException("지오코딩 변환 API 호출 중 오류가 발생하였습니다.", e);
         }
@@ -45,7 +52,11 @@ public class GeoCodingService {
             "&refine=true&simple=false&format=json&type=road&zipcode=true&key=" + apiKey;
 
         try {
-            return restTemplate.getForObject(requestUrl, GetAddressResponse.class);
+            GetAddressResponse response = restTemplate.getForObject(requestUrl, GetAddressResponse.class);
+            if (Objects.requireNonNull(response).getResponse().getStatus().equals(STATUS_NOT_FOUND)) {
+                throw new IllegalArgumentException("주소를에 해당하는 위,경도를 찾지 못했습니다.");
+            }
+            return response;
         } catch (RestClientException e) {
             throw new RestClientException("지오코딩 변환 API 호출 중 오류가 발생하였습니다.", e);
         }
